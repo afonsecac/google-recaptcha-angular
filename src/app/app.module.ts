d5b53c4ad3358e17fb089602d6ec3444b374600c
Alexander Fonseca <alexander.afonsecac@gmail.com>
2017-10-03 21:13:35 -0500

    Google ReCaptcha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ReCaptchaRenderDirective } from './re-captcha-render.directive';
+import { ReCaptchaGoogleCrearComponent } from './re-captcha-google-crear/re-captcha-google-crear.component';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   imports: [
-
+    CommonModule
   ],
-  declarations: [ReCaptchaRenderDirective]
+  declarations: [ReCaptchaRenderDirective, ReCaptchaGoogleCrearComponent],
+  exports: [ReCaptchaGoogleCrearComponent, ReCaptchaRenderDirective]
 })
 export class GoogleReCaptchaAngularModule { }
---
+++ src/app/re-captcha-google-crear/re-captcha-google-crear.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-re-captcha-google-crear',
+  templateUrl: './re-captcha-google-crear.component.html',
+  styleUrls: ['./re-captcha-google-crear.component.css']
+})
+export class ReCaptchaGoogleCrearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
