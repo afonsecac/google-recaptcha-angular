5b2f5e7e32e78647e3b77eee824f7b367aa9e04c
Alexander Fonseca <alexander.afonsecac@gmail.com>
2017-10-03 22:19:33 -0500

    Google ReCaptcha

--- src/app/re-captcha-google-crear/re-captcha-google-crear.component.ts
+++ src/app/re-captcha-google-crear/re-captcha-google-crear.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {ReCaptchaRenderDirective} from "../re-captcha-render.directive";
 
 @Component({
   selector: 'app-re-captcha-google-crear',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReCaptchaGoogleCrearComponent implements OnInit {
 
+  @Output('onCaptchaSubmit') onCaptchaSubmit: EventEmitter<any>;
+
+  @Input('key') key: string;
+  @Input('render') render: number;
+  @Input('recaptchaId') recaptchaId: string;
+
+  @ViewChild(ReCaptchaRenderDirective)
+  recaptchaInst: ReCaptchaRenderDirective;
+
   constructor() { }
 
   ngOnInit() {
+    this.onCaptchaSubmit = new EventEmitter();
+    this.render = 2500;
+    this.recaptchaId = 'grepcatcha';
+  }
+
+  recaptchaRestar() {
+    if (this.recaptchaInst != null) {
+      this.recaptchaInst.recaptchaRestar();
+    }
+  }
+
+  onCaptchaSubmited(data: string) {
+    if (data != null) {
+      this.onCaptchaSubmit.emit({
+          success: true,
+          token: data,
+          recaptcha: this
+      });
+    } else {
+      this.onCaptchaSubmit.emit({
+          success: false,
+          token: null,
+          recaptcha: this
+      });
+    }
   }
 
 }
